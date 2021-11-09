78c4148ddb741c078cc0c4fd6794842a2364fd3b
Malinowskik <74622359+Malinowskik@users.noreply.github.com>
2021-11-09 18:46:17 +0000

    Add files via upload

---
+++ 2.3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Console.WriteLine("Podaj swoją wagę w kilogramach: ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj swój wzrost w metrach: ");
+            b = double.Parse(Console.ReadLine());
+            c = a / (b * b);
+            Console.WriteLine("Twoje BMI wynosi " + c);
+            Console.ReadKey();
+        }
+    }
+}
