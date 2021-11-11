d455f813dbfb781f158aab2ab7941f45556fd9b2
Malinowskik <74622359+Malinowskik@users.noreply.github.com>
2021-11-11 22:31:59 +0000

    Add files via upload

---
+++ 3.15.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            for (int z1 = 0; z1 <= 10; z1++)
+                for (int z2 = 0; z2 <= 5; z2++)
+                    for (int z5 = 0; z5 <= 2; z5++)
+                        if (z1 * 1 + z2 * 2 + z5 * 5 == 10)
+                            Console.WriteLine("zlotowka = {0} dwa zlote = {1} piec zlote = {2}", z1, z2, z5);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 3.3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wprowadź 1 liczbe");
+            double l1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Wprowadź 2 liczbe");
+            double l2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Wybierz działanie: +, -, /, *");
+            string s = (Console.ReadLine());
+            if (s == "+")
+                Console.WriteLine("=", l1 + l2);
+            else if (s == "-")
+                Console.WriteLine("=", l1 - l2);
+            else if (s == "*")
+                Console.WriteLine("=", l1 * l2);
+            else if (s == "/")
+                Console.WriteLine("=", l1 / l2);
+            else
+                Console.WriteLine("Błędne dane");
+
+            Console.ReadKey();
+        }
+    }
+}
